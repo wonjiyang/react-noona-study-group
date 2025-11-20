5c7ce513ddcf699c56b0d4c2b81e42ae90081713
wonjiyang <ywj010827@naver.com>
2025-11-20 09:58:22 +0900

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import CustomNavbar from './components/Navbar/Navbar';
 import IntroMain from './pages/IntroMain/IntroMain';
+import Login from './pages/Login/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <Router>
+    <>
       <CustomNavbar />
       <Routes>
-        {' '}
-        <Route path="/" element={<IntroMain />} />{' '}
+        <Route path="/" element={<IntroMain />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
