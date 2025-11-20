7689ecb0e1981c1492513c5674a266c6ea6bdb76
jwjw7878 <jwp000708@gmail.com>
2025-11-21 08:45:03 +0900

    기능 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import CustomNavbar from './components/Navbar/Navbar';
-import IntroMain from './pages/IntroMain/IntroMain';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CustomNavbar from "./components/Navbar/Navbar";
+import IntroMain from "./pages/IntroMain/IntroMain";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MainPage from "./MainPage/MainPage";
 
 function App() {
   return (
-    <Router>
+    <>
       <CustomNavbar />
       <Routes>
-        {' '}
-        <Route path="/" element={<IntroMain />} />{' '}
+        {" "}
+        <Route path="/" element={<IntroMain />} />{" "}
+        <Route path="/main-page" element={<MainPage />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
