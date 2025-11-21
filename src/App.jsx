fb73132c74b106aae6997c6af443c5f5b35b3cdc
Hannah Jung <hj.hannahjung@gmail.com>
2025-11-20 16:45:25 -0800

    imported mainpage

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import MyPage from './pages/MyPage/MyPage';
 import QuestionList from './pages/MyPage/components/QuestionList/QuestionList';
 import BookmarkList from './pages/MyPage/components/BookmarkList/BookmarkList';
 import Login from './pages/LoginPage/Login';
+import MainPage from './MainPage/MainPage';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <CustomNavbar />
       <Routes>
         <Route path='/' element={<IntroMain />} />
+        <Route path='/main-page' element={<MainPage/>} />
         <Route path='/login' element={<Login />} />
         <Route path='/my' element={<MyPage />}>
           <Route path='questions' element={<QuestionList />} />
